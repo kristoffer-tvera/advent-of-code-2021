fb755eb7968cfcc92237fb0dccabbd29f42957b6
Kristoffer Tverå <Bluekristoffer@gmail.com>
2021-12-06 20:05:41 +0100

    Day 6

---
+++ AdventOfCode/Day6.cs
@@ -0,0 +1,74 @@
+﻿namespace AdventOfCode
+{
+    public class Day6 : Day, IDay
+    {
+        private readonly int NewFishDuration = 8;
+        private readonly int OldFishDuration = 6;
+        private readonly int InitialIterations = 80;
+        private readonly int MaxIterations = 256;
+        public Day6(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public async Task<string> Solution()
+        {
+            var input = await _http.GetStringAsync("/2021/day/6/input");
+            var fish = GetFish(input);
+
+            for (int i = 0; i < InitialIterations; i++)
+            {
+                var recycle = fish[0];
+                for (int j = 1; j < fish.Length; j++)
+                {
+                    fish[j - 1] = fish[j];
+                }
+
+                fish[OldFishDuration] += recycle;
+                fish[NewFishDuration] = recycle;
+            }
+
+            var part1Score = $"{CountFish(fish)}";
+
+            fish = GetFish(input);
+
+            for (int i = 0; i < MaxIterations; i++)
+            {
+                var recycle = fish[0];
+                for (int j = 1; j < fish.Length; j++)
+                {
+                    fish[j - 1] = fish[j];
+                }
+
+                fish[OldFishDuration] += recycle;
+                fish[NewFishDuration] = recycle;
+            }
+
+            var part2Score = $"{CountFish(fish)}";
+
+            return $"Part 1: {part1Score}, Part 2: {part2Score}";
+        }
+
+        private long CountFish(long[] fishes)
+        {
+            long count = 0;
+
+            foreach(var fish in fishes)
+            {
+                count += fish;
+            }
+
+            return count;
+        }
+
+        private long[] GetFish(string input)
+        {
+            var initialFishes = input.Split(',').Select(f => int.Parse(f));
+            var fish = new long[NewFishDuration + 1];
+            foreach (var initialFish in initialFishes)
+            {
+                fish[initialFish]++;
+            }
+            return fish;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,18 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
-//var day1 = new Day1(builder.Configuration);
-//var day2 = new Day2(builder.Configuration);
-//var day3 = new Day3(builder.Configuration);
-//var day4 = new Day4(builder.Configuration);
+var day1 = new Day1(builder.Configuration);
+var day2 = new Day2(builder.Configuration);
+var day3 = new Day3(builder.Configuration);
+var day4 = new Day4(builder.Configuration);
 var day5 = new Day5(builder.Configuration);
+var day6 = new Day6(builder.Configuration);
 
 var results = new Dictionary<string, string>();
-//results.Add("Day1", await day1.Solution());
-//results.Add("Day2", await day2.Solution());
-//results.Add("Day3", await day3.Solution());
-//results.Add("Day4", await day4.Solution());
+results.Add("Day1", await day1.Solution());
+results.Add("Day2", await day2.Solution());
+results.Add("Day3", await day3.Solution());
+results.Add("Day4", await day4.Solution());
 results.Add("Day5", await day5.Solution());
+results.Add("Day6", await day6.Solution());
 
 app.MapGet("/", () => "Hello World!\n" + string.Join("\n", results.Select(dict => $"{dict.Key}: {dict.Value}")));
 
